c7ae399e05bb029341e14c9236b2f8be77053a19
kazuhisa22144 <MKazuhisa22144@gmail.com>
2019-08-11 08:58:39 +0000

    header create3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,13 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   
 def after_sign_in_path_for(resource)
    	  if resource.is_a?(User)
- 	     user_path(resource)
+ 	     products_path(resource)
        elsif resource.is_a?(Admin)
 	     admin_root_path
       end
 end
 
-privated
+private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :phone_number ])
   end
