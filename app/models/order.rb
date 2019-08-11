07d33ed22e827acdf48b943beef4108065b9d7b0
sehatamanami <sehatamanami@gmail.com>
2019-08-11 19:57:08 +0900

    comments

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,9 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :order_products
 
-
+# defaultは1、"銀行振込"になる
   enum :payment_method_flag %i( クレジットカード　銀行振込　代引き )
+#  defaultは1、"受付中"になる
   enum :order_status_flag %i( 準備中　受付中　送付済み)
-  
+
 end
