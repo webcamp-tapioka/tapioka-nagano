0f287829182b264b492074bb57df79185f31973d
sehatamanami <sehatamanami@gmail.com>
2019-08-12 17:20:03 +0900

    to songs

--- app/controllers/admin/songs_controller.rb
+++ app/controllers/admin/songs_controller.rb
@@ -1,2 +1,22 @@
-class Admin::ListsController < ApplicationController
+class Admin::SongsController < ApplicationController
+ 
+
+  def create
+  end
+
+  def new
+  	@to_register = Song.new
+    @newsong = Song.new
+    if song_params
+    	@to_create_form = Song.new(params[:id])
+    end
+  end
+
+
+
+
+  private
+    def song_params
+  	  params.require(:song).permit(:product_id, :song, :track_number, :disc_number)
+  	end
 end
