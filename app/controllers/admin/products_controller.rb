c6e7c9f5da83826bc7cf9fce35d260efa5296c72
kazuhisa22144 <MKazuhisa22144@gmail.com>
2019-08-17 03:53:26 +0000

    products create1

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,49 @@
 class Admin::ProductsController < ApplicationController
+
+    def search
+    end
+
+
+	def index
+		@products = Product.all
+
+	end
+
+	def edit
+		@products = Product.find(params[:id])
+	end
+
+	def new
+		@products = Product.new
+	end
+
+	def create
+		@products = Products.new(products_params)
+	    products.save
+	    redirect_to products_params
+
+	end
+
+	def destroy
+		products = Products.find(params[:id])
+	end
+
+	def update
+		  @products = Products.find(params[:id])
+		if
+		  @products.update(products_params)
+		  redirect_to products_path(@products),notice: "Products was successfully updated"
+        else
+          flash[:notice] = "error"
+          render :edit
+        end
+	end
+
+ private
+  
+  def product_params
+    params.require(:products).permit(:image_id, :title, :artist, :stock, :price, :status )
+  end
+
+
 end
