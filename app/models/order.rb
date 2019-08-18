9455e9beff1b801ade8964057b5b955e34b76a4b
sehatamanami <sehatamanami@gmail.com>
2019-08-18 16:37:33 +0900

    controllers deleted

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_products
+  
 
 # defaultは1、"銀行振込"になる
   enum :payment_method_flag %i( クレジットカード　銀行振込　代引き )
@@ -8,3 +9,7 @@ class Order < ApplicationRecord
   enum :order_status_flag %i( 準備中　受付中　送付済み)
 
 end
+
+
+
+
