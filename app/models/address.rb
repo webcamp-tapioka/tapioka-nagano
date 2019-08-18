f0b0c52f2beb2d070a2e5f4e37092c9544fc2634
sehatamanami <sehatamanami@gmail.com>
2019-08-18 09:55:10 +0900

    addresses complete?

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,9 +4,4 @@ class Address < ApplicationRecord
 
 # default値は未設定。
   enum delivery_address_flag: %i( メイン住所 その他住所 )
-<<<<<<< HEAD
-
-=======
->>>>>>> df6690cb7482be38199cd034f5b099399552608a
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_17_103157) do
+ActiveRecord::Schema.define(version: 2019_08_17_124820) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_08_17_103157) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "delivery_address_flag", null: false
+    t.string "delivery_name_kana"
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
