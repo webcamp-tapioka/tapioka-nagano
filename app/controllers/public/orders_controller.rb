067d869f8745163fd120946a76e595724b640889
sehatamanami <sehatamanami@gmail.com>
2019-08-19 14:35:16 +0900

    disable...

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -7,7 +7,8 @@ class Public::OrdersController < ApplicationController
       address = current_user.addresses.find(params[:to_create_order].to_i)
       @new_order = current_user.orders.new(delivery_postal_code: address.delivery_postal_code, 
       delivery_address: address.delivery_address, delivery_name: address.delivery_name, 
-      delivery_name_kana: address.delivery_name_kana, postage: 0)
+      delivery_name_kana: address.delivery_name_kana, postage: PostageAndConsumptiontax.find(1).postage, 
+      total_price: current_user.total_price, consumption_tax: PostageAndConsumptiontax.find(1).consumption_tax)
     end
     current_user.cart_items.all.each do |cart_item|
       @new_order.order_products.build(product_id: cart_item.product_id, 
@@ -18,11 +19,16 @@ class Public::OrdersController < ApplicationController
 
 
   def create
-
-    
+    Order.create(order_params)
+    redirect_to users_thank_you_path
   end
+  
 
 
-
-
+  private
+    def order_params
+      params.require(:order).permit(:delivery_postal_code, :delivery_address, :delivery_name, :delivery_name_kana, 
+      :postage, :total_price, :consumption_tax, :payment_method, order_products_attributes: [:product_id, :order_amount, :price])
+    end
+    
 end
