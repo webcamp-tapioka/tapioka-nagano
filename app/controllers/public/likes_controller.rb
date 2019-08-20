32e0b1baaa685c0f3587154796b486a76a54d3a4
araki-takahiro <sanju13gh60ki@gmail.com>
2019-08-20 12:01:13 +0000

    ajax

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -1,11 +1,13 @@
 class Public::LikesController < ApplicationController
   def create
   	like = current_user.likes.create!(product_id: params[:product_id])
-  	redirect_to product_path(like.product_id)
+  	# redirect_to product_path(like.product_id)
+  	@products = Product.find(params[:product_id])
   end
 
   def destroy
-  	current_user.likes.find_by(params[:product_id]).destroy!
-  	redirect_to product_path(params[:product_id])
+  	@products = Product.find(params[:product_id])
+  	current_user.likes.find_by(product_id: params[:product_id]).destroy!
+  	# redirect_to product_path(params[:product_id])
   end
 end
