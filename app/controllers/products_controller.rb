5a15ea1626107cd38af937e07668a7602e20c286
sehatamanami <sehatamanami@gmail.com>
2019-08-26 12:02:01 +0900

    to pull

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,7 +14,7 @@ class ProductsController < ApplicationController
    end
    
 	def index
-		@products_all = Product.page(params[:page]).per(6)
+		@products_all = Product.where(deleted_at: nil).page(params[:page]).per(6)
   end
 
   def search
